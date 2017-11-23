3c358f3b233dae449483bd5976d194fbb107fd4a
Christian Romero <33636782+Christian-4@users.noreply.github.com>
2017-11-23 19:40:49 +0100

    Update Provincia.pl

--- Provincia.pl
+++ Provincia.pl
@@ -3,9 +3,8 @@ print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
    print "<b>Elige donde vives</b>";
    print "<form name=\"search\" method=\"get\">";
-   #$provincias="sevilla-valencia-madrid-barcelona";
-   @provincias1=("sevilla", "valencia", "madrid", "barcelona");
-   $pro{$provincias1[0]}=$provincias1[3];
+   @provincias=("sevilla", "valencia", "madrid", "barcelona");
+   $pro{$provincias[0]}=$provincias[3];
    print "<select name=\"provincia\">";
    for (keys %pro){
      print "<option value=\"$_\">$_</option>";
